dce57fec575a2e209f32924a4869512428c6b35f
a-zama <56331380+a-zama@users.noreply.github.com>
2020-02-05 01:14:31 +0900

    勝敗判定①

--- js/ttt.js
+++ js/ttt.js
@@ -1,159 +1,89 @@
 'use strict'
 
-// const box1 = document.getElementById('box1');
-// const box2 = document.getElementById('box2');
-// const box3 = document.getElementById('box3');
-// const box4 = document.getElementById('box4');
-// const box5 = document.getElementById('box5');
-// const box6 = document.getElementById('box6');
-// const box7 = document.getElementById('box7');
-// const box8 = document.getElementById('box8');
-// const box9 = document.getElementById('box9');
-
+// box を取得
 const boxes = document.querySelectorAll('.box');
+// Array に変換 ※IE11対策…らしい
+const boxesArray = [].slice.call(boxes);
 
+// message-b を取得
+const message = document.getElementById('message-b');
+
+// タテ・ヨコ・ナナメの列を配列化
+function filterById(targetArray, idArray) {
+  return targetArray.filter(function(e) {
+    return (e.id === idArray[0] || e.id === idArray[1] || e.id === idArray[2]);
+  });
+}
+
+const line1 = filterById(boxesArray, ['1-1', '1-2', '1-3']);
+const line2 = filterById(boxesArray, ['2-1', '2-2', '2-3']);
+const line3 = filterById(boxesArray, ['3-1', '3-2', '3-3']);
+const line4 = filterById(boxesArray, ['1-1', '2-1', '3-1']);
+const line5 = filterById(boxesArray, ['1-2', '2-2', '3-2']);
+const line6 = filterById(boxesArray, ['1-3', '2-3', '3-3']);
+const line7 = filterById(boxesArray, ['1-1', '2-2', '3-3']);
+const line8 = filterById(boxesArray, ['1-3', '2-2', '3-1']);
+
+const lineArray = [line1, line2, line3, line4, line5, line6, line7, line8];
+
+// 勝敗判定
+function isWinner(symbol) { 
+  const result = lineArray.some(function (line) {
+    const subResult = line.every(function (box) {
+      if (symbol === 'maru') {
+        return box.textContent.contains('〇');
+      } else 
+      if (symbol === 'batsu') {
+        return box.textContent.contains('×');
+      }
+    });
+    return subResult;
+  });
+  return result;
+}
+
+// flagがfalseのときバツのターン、trueのときマルのターン
 let flag =true;
 
 boxes.forEach((box) => {
   box.addEventListener('click', () => {
-  if(flag === true){
+  if (flag === true){
     box.classList.add('round');
     box.textContent = '〇';
+    
+    // マル勝利判定
+    if (isWinner('maru')) {
+      message.textContent = '〇の勝ち！';
+      return;
+    }
+    
+    message.textContent = '×の番です';
     flag = false;
-  }else{
+
+  } else {
     box.classList.add('cross');
     box.textContent = '×';
+
+     // バツ勝利判定
+     if (isWinner('batsu')) {
+      message.textContent = '×の勝ち！';
+      return;
+    }
+
+    message.textContent = '〇の番です';
     flag = true;
   }
+
 box.classList.add('td-notclickable');
 
+
+
+})
 })
 
-});
 
 
-// boxes.addEventListener('click', () => {
-//   if(flag === true){
-//     boxes.classList.add('round');
-//     boxes.textContent = '〇';
-//     flag = false;
-//   }else{
-//     boxes.classList.add('cross');
-//     boxes.textContent = '×';
-//     flag = true;
-//   }
-// boxes.classList.add('td-notclickable');
 
-// })
 
-// box1.addEventListener('click', () => {
-//   if(flag === true){
-//     box1.classList.add('round');
-//     box1.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box1.classList.add('cross');
-//     box1.textContent = '×';
-//     flag = true;
-//   }
-// box1.classList.add('td-notclickable');
-  
-// })
 
-// box2.addEventListener('click', () => {
-//   if(flag === true){
-//     box2.classList.add('round');
-//     box2.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box2.classList.add('cross');
-//     box2.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box3.addEventListener('click', () => {
-//   if(flag === true){
-//     box3.classList.add('round');
-//     box3.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box3.classList.add('cross');
-//     box3.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box4.addEventListener('click', () => {
-//   if(flag === true){
-//     box4.classList.add('round');
-//     box4.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box4.classList.add('cross');
-//     box4.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box5.addEventListener('click', () => {
-//   if(flag === true){
-//     box5.classList.add('round');
-//     box5.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box5.classList.add('cross');
-//     box5.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box6.addEventListener('click', () => {
-//   if(flag === true){
-//     box6.classList.add('round');
-//     box6.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box6.classList.add('cross');
-//     box6.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box7.addEventListener('click', () => {
-//   if(flag === true){
-//     box7.classList.add('round');
-//     box7.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box7.classList.add('cross');
-//     box7.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box8.addEventListener('click', () => {
-//   if(flag === true){
-//     box8.classList.add('round');
-//     box8.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box8.classList.add('cross');
-//     box8.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
-// box9.addEventListener('click', () => {
-//   if(flag === true){
-//     box9.classList.add('round');
-//     box9.textContent = '〇';
-//     flag = false;
-//   }else{
-//     box9.classList.add('cross');
-//     box9.textContent = '×';
-//     flag = true;
-//   }
-  
-// })
 
